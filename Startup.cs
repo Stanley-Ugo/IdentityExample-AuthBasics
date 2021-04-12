3bb9348d8868e6c8da2671859e4b7d3bd8318800
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-12 23:37:44 +0100

    Setting Up EntityFramework for dependency Injection

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using IdentityExample.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +18,10 @@ namespace IdentityExample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(config => {
+                config.UseInMemoryDatabase("Memory");
+            });
+
             //services.AddAuthentication("CookieAuth")
             //    .AddCookie("CookieAuth", config =>
             //    {
