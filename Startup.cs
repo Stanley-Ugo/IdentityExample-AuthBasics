99b42a3673ab1239ac6171bf645b3ef7275571df
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-06 20:51:51 +0100

    StartUp.cs Config file

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace IdentityExample
         {
             _config = config;
         }
-        // This method gets called by the runtime. Use this method to add services to the containe
+        // This method gets called by the runtime. Use this method to add services to the contain
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
