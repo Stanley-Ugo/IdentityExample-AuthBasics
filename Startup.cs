4e80411a5c277d416c66a4debaba31a97ec76e5c
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-06 20:53:40 +0100

    StartUp ConfigServices for Authenticationcookies

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace IdentityExample
         {
             _config = config;
         }
-        // This method gets called by the runtime. Use this method to add services to the 
+        // This method gets called by the runtime. Use this method to add services
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
