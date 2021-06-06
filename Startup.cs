699b1b16c20468058fc47c842fa3279f870c88ae
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-06 20:52:49 +0100

    StartUp ConfigServices

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace IdentityExample
         {
             _config = config;
         }
-        // This method gets called by the runtime. Use this method to add services to the cont
+        // This method gets called by the runtime. Use this method to add services to the co
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
