4bfcf70918c5ce084617ca9950718ad2ce383079
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-07 07:25:31 +0100

    Adding Required Lenght

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace IdentityExample
         {
             _config = config;
         }
-        // This method gets called by the runtime. Use this method to add servic
+        // This method gets called by the runtime. Use this method to add serv
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
