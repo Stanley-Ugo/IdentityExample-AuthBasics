180ab06461f49419cfdae7bacf1b063daf848473
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-08 03:21:01 +0100

    Registering services

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace IdentityExample
         {
             _config = config;
         }
-        // This method gets called by the runtime. Use this method to
+        // This method gets called by the runtime. Use this method
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
