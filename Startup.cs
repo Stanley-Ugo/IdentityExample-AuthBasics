4bd2ece6e23c772c8f76ddf49a3ee60288b1fe3b
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-08 03:21:51 +0100

    Adding Identity Service

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace IdentityExample
         {
             _config = config;
         }
-        // This method gets called by the runtime. Use this meth
+        // This method gets called by the runtime. Use this
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
