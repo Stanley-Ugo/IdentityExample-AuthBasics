b7c92d80ef79dbd24b3b6922d9aa136e84fa43dd
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-11 05:16:04 +0100

    Required Degit false

--- Startup.cs
+++ Startup.cs
@@ -34,8 +34,8 @@ namespace IdentityExample
 
             services.AddIdentity<IdentityUser, IdentityRole>(config => {
                 config.Password.RequiredLength = 4;
-                config.Password.RequireDigit = false
-                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireDigit = false;
+                config.Password.RequireNonAlphanumeric = false
                 config.Password.RequireUppercase = false;
                 config.SignIn.RequireConfirmedEmail = true;
             })
