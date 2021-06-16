26101dd03960b22f458fab0dad5f7e697e470f8f
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-17 00:10:58 +0100

    Adding Routing

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?Li
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
