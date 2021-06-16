b2f726b85e732c14f2206258c7b25a5cf52cd1fe
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-17 00:07:01 +0100

    Loading Application

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=39894
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
