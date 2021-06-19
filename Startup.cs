5bf5964382feb1df77a4369d1eb228140714ec3d
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-19 06:04:37 +0100

    Password COnfig

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more information on how to configure your application, visit http
+        // For more information on how to configure your application, visit ht
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
