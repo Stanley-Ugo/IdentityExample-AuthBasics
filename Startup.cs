5378aba461f5800119a50873eab9b611fd7816f8
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-20 00:05:54 +0100

    Configuring Section

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more information on how to configure your app
+        // For more information on how to configure your a
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
