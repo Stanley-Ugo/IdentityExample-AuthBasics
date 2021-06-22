dbfa1f12b765e922a24d8f823bfae69e6179f8cd
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-22 19:10:55 +0100

    Adding Parameters

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more information on ho
+        // For more information on h
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
