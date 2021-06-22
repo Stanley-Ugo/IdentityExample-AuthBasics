ab87fa8de70dc148de88b661f85afa3cc8f52809
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-23 00:54:26 +0100

    Password Configuration Uppercase

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more in
+        // For more 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
