d89cc6f5f3079b5eb8f6e9d0c1137d32d73cc2cf
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-22 19:09:47 +0100

    Cooke configuration

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more information on how t
+        // For more information on how 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
