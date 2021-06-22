b07ba530137953a342b40e6a3a8bb28299932ab7
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-23 00:52:18 +0100

    Password Configuration Lenght

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more infor
+        // For more info
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
