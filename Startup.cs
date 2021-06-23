8414c04a1884d4d50b8623fdc1aca5de66919ef2
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-24 00:23:49 +0100

    removing using generics

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NETCore.MailKit.Extensions;
 using NETCore.MailKit.Infrastructure.Internal;
-using System.Collections.Generic;
+using System.Collections.Gener
 using System.Linq;
 using System.Threading.Tasks;
 
