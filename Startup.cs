dd8b9b3e6152f4f07035d4512eb957ee1319ffd8
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-25 00:01:41 +0100

    Loading Application II

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For more
+        // For mor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
