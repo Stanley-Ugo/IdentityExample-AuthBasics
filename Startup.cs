44a6254f59772868f0ebb86de8fa9ab83f2e74cd
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-25 00:02:20 +0100

    Adding Comments to startup.cs file

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace IdentityExample
             _config = config;
         }
         
-        // For
+        // F
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(config => {
