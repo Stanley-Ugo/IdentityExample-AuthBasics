55ebe126c9692af6131f43c2b9bffa7bc88f279c
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-07-03 00:20:51 +0100

    Redirecting to Email verification

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -100,9 +100,9 @@ namespace IdentityExample.Controllers
 
                 var link = Url.Action(nameof(VerifyEmail), "Home", new { userId = user.Id, code }, Request.Scheme, Request.Host.ToString());
 
-                await _emailService.SendAsync("test@test.com", "Email Verify", $"<a href=\"{link}\">Verify Email</a>", true)
+                await _emailService.SendAsync("test@test.com", "Email Verify", $"<a href=\"{link}\">Verify Email</a>", true);
 
-                return RedirectToAction("EmailVerification");
+                return RedirectToAction("EmailVerification")
             }
             return RedirectToAction("Index");
         }
