4935a6624156dc14a95cc8d0aca002f366aaeebe
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-07-04 18:14:45 +0100

    Adding AspNetCore.Authorization

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentica
+﻿using Microsoft.AspNetCore.Authenti
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
