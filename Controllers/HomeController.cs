60e66e1c8bcbec700cd367ea1b60eacbdcc3b28c
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-07-06 00:10:30 +0100

    Building Applicaion

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NETCore.MailKit.Core;
 using System;
-using 
 using System.Security.Claims;
 using System.Threading.Tasks;
 
