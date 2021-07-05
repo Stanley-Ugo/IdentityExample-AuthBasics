90801f47945dd5e70c7f47ce49990e795ba4c46d
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-07-06 00:10:11 +0100

    Adding Using System.Claims

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NETCore.MailKit.Core;
 using System;
-using Syste
+using 
 using System.Security.Claims;
 using System.Threading.Tasks;
 
