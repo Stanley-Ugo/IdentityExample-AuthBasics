946d6ac437362f10b5f6ee1036f16ed50479e950
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-07-07 06:13:55 +0100

    Register method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -71,7 +71,7 @@ namespace IdentityExample.Controllers
             return RedirectToAction("Index");
         }
 
-        //Register A method
+        //Register Act method
         public IActionResult Register()
         {
             return View();
