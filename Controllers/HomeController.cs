49c9a539e7438dc67418a315e3e885ac544bae88
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-07-08 18:38:03 +0100

    loading application I

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NETCore.MailKit.Core;
 using System;
-using System.Security.Claims
+using System.Security.Claim
 using System.Threading.Tasks;
 
 namespace IdentityExample.Controllers
